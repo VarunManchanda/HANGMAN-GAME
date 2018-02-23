2fe8bc09f5540cf5e878152f822c5f25dd64aba4
VarunManchanda <33371013+VarunManchanda@users.noreply.github.com>
2018-02-24 03:10:16 +0530

    Add files via upload

--- Hangman.java
+++ Hangman.java
@@ -1,27 +1,33 @@
                                                         /*HANGMAN GAME
                                                     Created By:- Varun Manchanda
-                                                      50% Created till Now!
+                                                          100% Created!!
                                                             */
 package Hangman;
 import java.util.Random;
 import java.util.Arrays;
+import java.util.Scanner;
 public class Hangman{
 	
 static String hang(){
-String[] s1 = {"ELASTICITY", "ADDICTIONS","ADJUSTABLE","ADVOCATORS"};
+String[] s1 = {"ELASTICITY", "ADDICTIONS","ADJUSTABLE","ADVOCATORS", "CONGRATULATION", "BACKBENCHERS", "NEVADA"};
 Random obj1 = new Random();
 int x = obj1.nextInt(s1.length)+0;
 return s1[x];
 }	
 
 static void display(){
-	System.out.println("*******************WELCOME TO HANGMAN GAME*******************"); 
+	System.out.println("*******************WELCOME TO HANGMAN GAME*******************");
+	System.out.println("                         (  @   @   )                        ");
 	System.out.println("                              |                              ");
-    System.out.println("                              |                              ");
+    System.out.println("                    ------    |   ------                     ");
 	System.out.println("                              |                              ");
 	System.out.println("                            // \\                             ");
 	System.out.println("                           //   \\                            ");
 	System.out.println("                          //     \\                           ");	
+}
+
+static void display1(){
+	System.out.println("***************************THE END***************************"); 
 }
 
 public static void main(String[] args){
@@ -33,6 +39,10 @@ public static void main(String[] args){
 	// String arr = "ELASTICITY";
 	 String name = hang();
 	 char ch[] = name.toCharArray();
+	 char w;
+	 char game[] = new char[4];
+	 int total = 0;
+	 Scanner word = new Scanner(System.in);
 //Initialization:-     
      int max = 5;
 	 int temp=0, l=0;
@@ -71,6 +81,50 @@ for(int j=0; j<ch.length; j++){
 		System.out.print(" "+ch[j]);
 	}
 		
+}
+//START THE GAME:-
+
+for(l=0; l<4; l++){
+	for(int i=l; i<=l; i++){
+		game[l] = name.charAt(fan[i]);
+	}
+}
+/*Testing Purpose:-
+System.out.println("Words are:-");
+for(int i=0; i<4; i++){
+	System.out.println(game[i]);
+}
+*/
+//Calculation Begins:-
+String t;
+System.out.println("");
+System.out.println("Now Guess The Missing Letters");
+for(int i=0; i<4; i++){
+	System.out.println("Enter the "+(i+1)+" Letter");
+	t = word.next();
+	w = t.charAt(0);
+	if(game[i]==w){
+		total += 5;
+	//System.out.println("total is "+total);	
+	System.out.println("Right Answer word is "+game[i]);
+	
+		continue;
+	}
+	else{
+		total -= 2;
+		System.out.println("Wrong Answer, try Again");
+		i--;
+	}
+	
+}
+if(total==20){
+	System.out.println("Congratulation's you Win! and the Words is "+name);
+	display1();
+}
+else{
+	System.out.println("Your Total Score is "+total);
+	System.out.println("Better Luck Next Time");
+	display1();
 }
 
 
